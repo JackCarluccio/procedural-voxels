f83d1ba435de2833a8f55621a48091ba1c4bee9f
JackCarluccio <jcarluccio2006@gmail.com>
2026-04-07 01:28:51 -0400

    feat: add PerlinNoise2d

---
+++ include/world/generation/perlin_noise_2d.h
@@ -0,0 +1,8 @@
+#ifndef VOXELS_WORLD_GENERATION_PERLIN_NOISE_2D_H
+#define VOXELS_WORLD_GENERATION_PERLIN_NOISE_2D_H
+
+namespace voxels::world::generation {
+    float PerlinNoise2d(float x, float y) noexcept;
+}
+
+#endif // VOXELS_WORLD_GENERATION_PERLIN_NOISE_2D_H
--- src/world/chunk_generator.cc
+++ src/world/chunk_generator.cc
@@ -1,27 +1,45 @@
 #include "world/chunk_generator.h"
 
 #include "world/block.h"
+#include "world/generation/perlin_noise_2d.h"
 
 #include <cmath>
 
 namespace voxels::world {
 
-void ChunkGenerator::Generate(Chunk& chunk) {
-    static constexpr int AMPLITUDE = 16;
-    static constexpr int OFFSET = 16;
-    static constexpr float FREQUENCY = 0.1f;
+namespace {
 
+constexpr int AMPLITUDE = 16;
+constexpr int BASE_HEIGHT = 16;
+constexpr float FREQUENCY = 0.1f;
+
+void GenerateHeightMap(int* heightMap, int chunkWorldX, int chunkWorldZ) {
+    for (int x = 0; x < Chunk::SIZE; x++) {
+        for (int z = 0; z < Chunk::SIZE; z++) {
+            int height = static_cast<int>(
+                generation::PerlinNoise2d(
+                    (x + chunkWorldX) * FREQUENCY,
+                    (z + chunkWorldZ) * FREQUENCY
+                ) * AMPLITUDE + BASE_HEIGHT
+            );
+
+            heightMap[z + x * Chunk::SIZE] = height;
+        }
+    }
+}
+
+} // namespace
+
+void ChunkGenerator::Generate(Chunk& chunk) {
     int chunkWorldX = chunk.GetPosition().x * Chunk::SIZE;
     int chunkWorldZ = chunk.GetPosition().y * Chunk::SIZE;
 
+    int heightMap[Chunk::BLOCKS_PER_SLICE];
+    GenerateHeightMap(heightMap, chunkWorldX, chunkWorldZ);
+
     for (int x = 0; x < Chunk::SIZE; x++) {
         for (int z = 0; z < Chunk::SIZE; z++) {
-            int terrainHeight = static_cast<int>(
-                (
-                    sin((x + chunkWorldX) * FREQUENCY) +
-                    cos((z + chunkWorldZ) * FREQUENCY)
-                ) * AMPLITUDE + OFFSET
-            );
+            int terrainHeight = heightMap[z + x * Chunk::SIZE];
 
             for (int y = 0; y < Chunk::HEIGHT; y++) {
                 Block block;
